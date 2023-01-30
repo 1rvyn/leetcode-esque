1090e0853dec02abac9a2a90e3bf977d195afe40
irvyn <i.hall3@rgu.ac.uk>
2023-01-30 16:47:11 +0000

    updates

--- views/public/js/register.js
+++ views/public/js/register.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
     // Get and set element 'form'
-    const form = document.querySelector(".login-form");
+    const form = document.querySelector(".register-form");
 // Get and set element 'email'
     const email = document.querySelector("#email");
 // Get and set element 'password'
     const password = document.querySelector("#password");
     const name = document.querySelector("#name");
-// Add an event listener to the login button and use the api to process login event
+// Add an event listener to the register button and use the api to process regster event
     form.addEventListener("submit", async (e) => {
         e.preventDefault();
         // display.textContent = "";
         console.log(password.value + email.value)
         try {
             const res = await fetch("https://irvyn.dev/api/register", {
-                // const res = await fetch("/api/login", {
+                // const res = await fetch("/api/register", {
                 method: "POST",
                 headers: {"Content-Type": "application/json",
                             "Accept": "application/json"},
@@ -35,9 +35,8 @@ $(document).ready(function(){
                 // append message to page
             }
             else if (res.status === 200){
-                console.log("the login has a success response code good job :)")
+                console.log("the register has a success response code good job :)")
                 // print out the cookie 
-                console.log("cookie is: ", document.cookie)
                 console.log(res, content)
                 // window.location.href = "/";
 
