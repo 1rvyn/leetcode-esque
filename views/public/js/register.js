9f2d232c6661fbc83223af05b168f7cb1237dd70
irvyn <i.hall3@rgu.ac.uk>
2023-01-30 16:44:01 +0000

    switch register function

--- views/public/js/register.js
+++ views/public/js/register.js
@@ -1,48 +1,37 @@
-    console.log("this should print on the register page")
+// Get form values
+const form = $(".register-form");
+const email = $("#email").val();
+const name = $("#name").val();
+const password = $("#password").val();
 
-    $(document).ready(function(){
-        const form = document.querySelector(".register-form");
-        const email = document.querySelector("#email");
-        const name = document.querySelector("#name");
-        const password = document.querySelector("#password");
-    // Add an event listener to the login button and use the api to process login event
-        form.addEventListener("submit", async (e) => {
-            e.preventDefault();
-            // display.textContent = "";
-            console.log(password.value + email.value + name.value)
-            try {
-                const res = await fetch("https://irvyn.dev/api/register", {
-                    // const res = await fetch("/api/register", {
-                    method: "POST",
-                    headers: {"Content-Type": "application/json",
-                        "Accept": "application/json",
-                   "Access-Control-Allow-Origin": "irvyn.xyz"
-                 },
-                    body: JSON.stringify({
-                        name: name.value,
-                        email: email.value,
-                        password: password.value,
-                    }),
-                    xhrFields: {
-                        withCredentials: true,
+// Set up request body
+const requestBody = {
+  email: email,
+  name: name,
+  password: password
+};
 
-                    },
-                    credentials: "include",
-                });
-                const content = await res.json();
-                console.log(content);
-                console.log(res.status);
-                if (res.status === 400 || res.status === 401) {
-                    console.log("there was an issue")
-                }
-                else if (res.status === 200){
-                    console.log("content inside is:", content)
+// Set up headers
+const headers = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*"
+};
 
-                    console.log("the login has a success response code good job :)")
-                    // window.location.href = "/login";
-                }
-            } catch (err) {
-                console.log(err.message);
-            }
-        });
-    })
+// Send POST request to API
+$.ajax({
+  type: "POST",
+  url: "https://irvyn.dev/api/register",
+  headers: headers,
+  data: JSON.stringify(requestBody),
+  success: function(data) {
+    console.log(data);
+  },
+  error: function(error) {
+    console.error("There was a problem with the request:", error);
+  }
+});
+
+// Prevent form from refreshing the page
+form.submit(function(event) {
+  event.preventDefault();
+});
