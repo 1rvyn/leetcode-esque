fb9af401ff831e5eb1656721241a611cd2240828
irvyn <i.hall3@rgu.ac.uk>
2023-02-07 11:00:06 +0000

    get submissions button + request

--- views/public/js/account.js
+++ views/public/js/account.js
@@ -48,3 +48,45 @@ window.onload = async function() {
     }
     };
 
+    async function getSubmissions() {
+        console.log("clicked get submissions button");
+        try{
+            const res = await fetch("https://api.irvyn.xyz/submissions", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                xhrFields: {
+                    withCredentials: true
+                },
+                credentials: "include"
+            });
+            const content = await res.json();
+            console.log(content);
+            console.log(res.status);
+            if (res.status === 200) {
+                const submissionsDiv = document.querySelector(".submissions");
+                let submissionsHtml = "";
+                for (let i = 0; i < content.length; i++) {
+                    submissionsHtml += "<div class='submission'>";
+                    submissionsHtml += "<p>Submission ID: " + content[i].id + "</p>";
+                    submissionsHtml += "<p>Submission Code: " + content[i].code + "</p>";
+                    submissionsHtml += "<p>Submission Output: " + content[i].output + "</p>";
+                    submissionsHtml += "<p>Submission Error: " + content[i].error + "</p>";
+                    submissionsHtml += "<p>Submission Created At: " + content[i].created_at + "</p>";
+                    submissionsHtml += "</div>";
+                }
+                submissionsDiv.innerHTML = submissionsHtml;
+            } else {
+                console.log("There was an error retrieving data from the API");
+            }
+
+        }
+        catch (err) {
+            console.log(err.message);
+        }
+    }
+
+
+
