95a5a042a7306d28d7633932c676641d1734eadd
1rvyn <i.hall3@rgu.ac.uk>
2023-03-18 08:55:37 +0000

    add on click listener + new POST in prev format

--- views/public/js/addquestion.js
+++ views/public/js/addquestion.js
@@ -1,22 +1,46 @@
-const form = document.querySelector('form');
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('submit-question-form');
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
+    form.addEventListener('submit', async function (event) {
+        event.preventDefault();
 
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
+        const formData = new FormData(form);
+        const requestData = {
+            problem: formData.get('problem'),
+            example_answer: formData.get('example_answer'),
+            example_input: formData.get('example_input'),
+            problem_type: formData.get('problem_type'),
+            problem_difficulty: formData.get('problem_difficulty'),
+            template_code: {
+                python: formData.get('template_code[python]'),
+                javascript: formData.get('template_code[javascript]'),
+                go: formData.get('template_code[go]')
+            }
+        };
+        console.log(requestData);
+        try {
+            const res = await fetch('/submit_question', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(requestData),
+                credentials: 'include'
+            });
+            const content = await res.json();
 
-    fetch('https://api.irvyn.xyz/add', {
-        method: 'POST',
-        headers: {"Content-Type": "application/json",
-            "Accept": "application/json"},
-        body: JSON.stringify(data),
-        xhrFields: {
-            withCredentials: true
-        },
-        credentials: "include",
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error(error));
+            if (res.status === 400 || res.status === 401) {
+                console.log("There was an issue");
+            } else if (res.status === 200) {
+                console.log("The form submission has a success response code, good job :)");
+            }
+
+            // Handle the response content here
+            // e.g., display a success message, redirect, or update the page content
+
+        } catch (err) {
+            console.log(err.message);
+        }
+    });
 });
