467e8732b9813068fdfecbde435e0f310e99a66c
1rvyn <i.hall3@rgu.ac.uk>
2023-04-28 12:57:45 +0100

    fix createdAt format

--- views/public/js/account.js
+++ views/public/js/account.js
@@ -2,36 +2,51 @@ console.log("this should print if we are on the account page");
 
 window.onload = async function() {
     try {
-    const res = await fetch("https://api.irvyn.xyz/account", {
-    method: "POST",
-    headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-    },
-    xhrFields: {
-    withCredentials: true
-    },
+        const res = await fetch("https://api.irvyn.xyz/account", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+            xhrFields: {
+            withCredentials: true
+            },
     credentials: "include"
     });
-    const content = await res.json();
-    console.log(content);
-    console.log(res.status);
+        const content = await res.json();
+        console.log(content);
+        console.log(res.status);
     if (res.status === 200) {
-    const accountInfoDiv = document.querySelector(".account-info");
-    let accountInfoHtml = "";
-    accountInfoHtml += "<p>ID: " + content.id + "</p>";
-    accountInfoHtml += "<p>Name: " + content.name + "</p>";
-    accountInfoHtml += "<p>Email: " + content.email + "</p>";
-    accountInfoHtml += "<p>Created At: " + content.created_at + "</p>";
-    accountInfoHtml += "<p>User Role: " + content.UserRole + "</p>";
-    accountInfoDiv.innerHTML = accountInfoHtml;
+        let formattedCreatedAt = formatDate(content[i].created_at);
+        const accountInfoDiv = document.querySelector(".account-info");
+        let accountInfoHtml = "";
+        accountInfoHtml += "<p>ID: " + content.id + "</p>";
+        accountInfoHtml += "<p>Name: " + content.name + "</p>";
+        accountInfoHtml += "<p>Email: " + content.email + "</p>";
+        accountInfoHtml += "<p>Created At: " + formattedCreatedAt+ "</p>";
+        accountInfoHtml += "<p>User Role: " + content.UserRole + "</p>";
+        accountInfoDiv.innerHTML = accountInfoHtml;
     } else {
-    console.log("There was an error retrieving data from the API");
+        console.log("There was an error retrieving data from the API");
     }
     } catch (err) {
-    console.log(err.message);
+        console.log(err.message);
     }
+};
+
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZoneName: 'short',
     };
+    return date.toLocaleString('en-US', options);
+}
+
 
     
       
@@ -56,13 +71,15 @@ window.onload = async function() {
                 const submissionsDiv = document.querySelector(".submissions");
                 let submissionsHtml = "";
                 for (let i = 0; i < content.length; i++) {
-                    submissionsHtml += "<div class='submission'>";
-                    submissionsHtml += "<p>Submission ID: " + content[i].id + "</p>";
-                    submissionsHtml += "<p>Submission Code: " + content[i].code + "</p>";
-                    submissionsHtml += "<p>Submission Output: " + content[i].output + "</p>";
-                    submissionsHtml += "<p>Submission Error: " + content[i].error + "</p>";
-                    submissionsHtml += "<p>Submission Created At: " + content[i].created_at + "</p>";
-                    submissionsHtml += "</div>";
+                    const formattedCreatedAt = formatDate(content[i].created_at);
+
+                    submissionsHtml += `<div class='submission bg-gray-100 my-4 p-4 rounded-lg shadow-md'>`;
+                    submissionsHtml += `<p class='font-bold text-gray-700'>Submission ID: <span class='font-normal'>${content[i].id}</span></p>`;
+                    submissionsHtml += `<p class='font-bold text-gray-700'>Submission Code: <span class='font-normal'>${content[i].code}</span></p>`;
+                    submissionsHtml += `<p class='font-bold text-gray-700'>Submission Output: <span class='font-normal'>${content[i].output}</span></p>`;
+                    submissionsHtml += `<p class='font-bold text-gray-700'>Submission Error: <span class='font-normal'>${content[i].error}</span></p>`;
+                    submissionsHtml += `<p class='font-bold text-gray-700'>Submission Created At: <span class='font-normal'>${formattedCreatedAt}</span></p>`;
+                    submissionsHtml += `</div>`;
                 }
                 submissionsDiv.innerHTML = submissionsHtml;
             } else {
